f3096a295f7195faa45398edbf43d56f379dfdd2
Charanjeev Kaur <69391607+Charanjeev-2k2@users.noreply.github.com>
2021-07-06 15:40:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidV4 } = require('uuid');
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
+  res.redirect(`/${uuidV4()}`);
+});
+
+
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+  res.render('room', { roomId: req.params.room });
+});
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit('user-connected', userId);
 
+    
+
+    //disconnect
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
-    })
-  })
-})
+      socket.broadcast.to(roomId).emit('user-disconnected', userId);
+    });
+
+
+    //for sending chat message
+    socket.on("send_msg", (message) => {
+      //for displaying it on front end
+      io.to(roomId).emit('create_message', message);
+    });
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
